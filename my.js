7c4a85972d1a19ec41242335fc326677bc5c239d
zhuhe <zhuhe@changingedu.com>
2018-04-23 17:31:03 +0800

    create project

---
+++ my.js
@@ -0,0 +1,6 @@
+console.log("mmmmmmy  js");
+var title=document.getElementsByClassName('QuestionHeader-title');
+console.log(title.length);
+for(i=0;i<title.length;i++){
+	title[i].textContent='';
+}
