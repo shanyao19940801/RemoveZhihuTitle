983e26c069d249a21980eb783ca3202998e00c71
zhuhe <zhuhe@changingedu.com>
2018-04-23 17:38:26 +0800

    remove unused code

--- my.js
+++ my.js
@@ -1,6 +1,4 @@
-console.log("mmmmmmy  js");
 var title=document.getElementsByClassName('QuestionHeader-title');
-console.log(title.length);
 for(i=0;i<title.length;i++){
 	title[i].textContent='';
 }
