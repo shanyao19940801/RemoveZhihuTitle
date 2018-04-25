1e5b7f4e8977bdc38ed0ca76438292c915b7cc00
zhuhe <zhuhe@changingedu.com>
2018-04-25 19:39:50 +0800

    add omnibox search

--- background.js
+++ background.js
@@ -11,3 +11,47 @@ chrome.runtime.onInstalled.addListener(function() {
     }]);
   });
 });
+
+
+chrome.omnibox.onInputChanged.addListener(
+  function(text, suggest) {
+    updateDefaultSuggestion(text);
+        results.push({
+          content: path + '@' + line,
+          description: description
+        });
+      suggest(results);
+
+
+      chrome.bookmarks.getTree(function what(results){
+        console.log("aaa");
+          console.log(results);
+      });
+    })
+
+
+
+function resetDefaultSuggestion() {
+  chrome.omnibox.setDefaultSuggestion({
+    description: '<url><match>快捷查找书签:</match></url> Search Chromium source'
+  });
+}
+
+function updateDefaultSuggestion(text) {
+  var description = '<match><url>快捷查找书签</url></match><dim> :</dim>';
+  description +='<match>' + text + '</match>';
+
+  chrome.omnibox.setDefaultSuggestion({
+    description: description
+  });
+}
+
+resetDefaultSuggestion();
+
+chrome.omnibox.onInputStarted.addListener(function() {
+  updateDefaultSuggestion('');
+});
+
+chrome.omnibox.onInputCancelled.addListener(function() {
+  resetDefaultSuggestion();
+});
