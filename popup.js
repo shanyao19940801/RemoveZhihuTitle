9a4e9d769500badbae54bc40e6d5c5edb28d104a
zhuhe <zhuhe@changingedu.com>
2018-04-25 10:31:44 +0800

    remove unused code

--- popup.js
+++ popup.js
@@ -1,40 +1,10 @@
-// Copyright 2018 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-console.log("init popup.js");
 
 let changeColor = document.getElementById('changeColor');
-
-// chrome.storage.local.get('color', function(data) {
-//   changeColor.style.backgroundColor = data.color;
-//   changeColor.setAttribute('value', data.color);
-// });
 
 var switchKey="zhihu_switch_zh"
 
 changeColor.onclick = function() {
-//   chrome.storage.local.get([switchKey], function(data) {
-//   	console.log(data);
-// 	if(data.key){
-// 		chrome.storage.local.set({switchKey:true},function(){
-// 			// console.log("value set "+!data.value);
-// 			// if(!data.value){
-//    //        		changeColor.setAttribute('style','background-color: green');
-// 			// }
-// 			// else{
-//    //        		changeColor.setAttribute('style','background-color: red');
-// 			// }
-// 		});
-// 	}else{
-// 		//init
-// 		chrome.storage.local.set({switchKey: false}, function() {
-//           console.log('Value is set to false');
-//           changeColor.setAttribute('style','background-color: red');
-//         });
-// 	}
 
-// });
     chrome.storage.local.get([ switchKey], function(items) {
     	console.log(items);
         var options = items;
@@ -55,16 +25,6 @@ changeColor.onclick = function() {
         chrome.storage.local.set(options);
     });
 };
-
-
-
-// function switch(){
-// 	var value=localStorage.getItem(switchKey);
-// 	console.log(value);
-// 	if(value!=nudefined){
-// 		localStorage.setItem(!value);
-// 	}
-// }
 
 function switchRemove() {
 chrome.storage.local.get([switchKey], function(data) {
